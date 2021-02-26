147defd888e9d09a40d7ffd981e8b1d22ab0ec07
GitHub Ryanxiejh <GitHub 294732246@qq.com>
2021-02-26 21:06:47 +0800

    sycl parallel_reduce(TeamPolicy) test1

--- kokkos-master/core/src/SYCL/Kokkos_SYCL_Parallel.hpp
+++ kokkos-master/core/src/SYCL/Kokkos_SYCL_Parallel.hpp
@@ -805,14 +805,14 @@ public:
         const auto reduction = [&]() {
             if constexpr (!std::is_same<ReducerType, InvalidType>::value) {
                 printf("ReducerType: Built-in Type !!! \n");
-//                return cl::sycl::ONEAPI::reduction(
-//                        result_ptr, identity,
-//                        [=](value_type& old_value, const value_type& new_value) {
-//                            m_reducer.join(old_value, new_value);
-//                            return old_value;
-//                        });
-                return cl::sycl::ONEAPI::reduction(result_ptr, identity,
-                                                   std::plus<>());
+                return cl::sycl::ONEAPI::reduction(
+                        result_ptr, identity,
+                        [=](value_type& old_value, const value_type& new_value) {
+                            m_reducer.join(old_value, new_value);
+                            return old_value;
+                        });
+//                return cl::sycl::ONEAPI::reduction(result_ptr, identity,
+//                                                   std::plus<>());
             } else {
                 if constexpr (ReduceFunctorHasJoin<Functor>::value) {
                     printf("ReducerType: Custome Type !!! \n");
